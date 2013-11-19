7e4eb4194d86529909380f33753648f857e593db
Kalle Alm <kalle.alm@gmail.com>
2013-11-19 07:01:24 +0100

    Added Helping out section

--- src/Pajdeg.h
+++ src/Pajdeg.h
@@ -56,6 +56,12 @@
  @section integrating_sec Integrating
  
  Adding Pajdeg to your project can be done by either compiling the static library using the provided Makefile, and putting the library and the .h files into your project, or by adding the .c and .h files directly. 
+ 
+ @section Helping out
+ 
+ One of the design goals with Pajdeg is that it should have minimal dependencies and be as broadly available as possible. As a consequence of this, some core components have been implemented in a rather crude form and are in desperate need of optimizing, in particular
+ 
+ - the binary tree implementation is not balanced (pd_btree)
  */
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
