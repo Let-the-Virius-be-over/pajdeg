1932ea936012461d6087c798bcbb14b4331e4b20
Kalle Alm <kalle.alm@gmail.com>
2014-07-26 20:49:25 +0200

    Version bump for pod spec fixes.

--- src/Pajdeg.h
+++ src/Pajdeg.h
@@ -369,7 +369,7 @@
 #ifndef INCLUDED_PAJDEG_H
 #   define INCLUDED_PAJDEG_H
 
-#   define PAJDEG_VERSION   "0.0.1"
+#   define PAJDEG_VERSION   "0.0.2"
 
 #   include "PDPipe.h"
 #   include "PDObject.h"
